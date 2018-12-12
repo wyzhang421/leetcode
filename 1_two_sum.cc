c5302163eabde51a0c0f39e7ddc4726146998ac4
wyzhang <zhangwanying421@gmail.com>
2018-12-12 22:48:03 +0800

    2018/12/12 add solution for Two Sum

---
+++ 1_two_sum.cc
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int, int> mp;
+        vector<int> ans;
+        for(int i = 0; i < nums.size(); ++i){
+            mp[nums[i]] = i;
+        }
+        for(int i = 0; i < nums.size(); ++i){
+            if(mp.find(target - nums[i])!=mp.end() && mp[target - nums[i]] > i){
+                ans.push_back(i);
+                ans.push_back(mp[target-nums[i]]);
+                break;
+            }
+        }
+        return ans;
+    }
+};
